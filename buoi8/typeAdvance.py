ba2fe280eae31c3aa237dcd602af0e4de6501113
phucnguyen070995 <p070995@gmail.com>
2020-12-05 23:16:08 +0700

    doing type advance3

--- buoi8/typeAdvance.py
+++ buoi8/typeAdvance.py
@@ -1,293 +1,54 @@
-#cau 1
 class StaticCheck(Visitor):
     def visitProgram(self,ctx:Program,o):
-        o = {}
+        o = [{}, {}]
         vardecl = [self.visit(x,o) for x in ctx.decl]
         stmt = [self.visit(x,o) for x in ctx.stmts]
-        
+
     def visitVarDecl(self,ctx:VarDecl,o):
-        o[ctx.name] = ctx.name
-        
-    def visitAssign(self,ctx:Assign,o):
-        rtype = self.visit(ctx.rhs,o)
-        ltype = self.visit(ctx.lhs,o)
-        if ltype not in ['int', 'float', 'bool']:
-            if rtype not in ['int', 'float', 'bool']:
-                raise TypeCannotBeInferred(ctx)
-            o[ltype] = rtype
-            ltype = rtype
-        if rtype not in ['int', 'float', 'bool']:
-            o[rtype] = ltype
-            rtype = ltype
-        if ltype != rtype:
-            raise TypeMismatchInStatement(ctx)
-
-
-    def visitBinOp(self,ctx:BinOp,o):
-        operator = ctx.op
-        ltype = self.visit(ctx.e1,o)
-        rtype = self.visit(ctx.e2,o)
-        if operator in ['+', '-', '*','/']:
-            if ltype in ['bool', 'float'] or rtype in ['bool', 'float']:
-                raise TypeMismatchInExpression(ctx)
-            if ltype != 'int':
-                o[ltype] = 'int'
-            if rtype != 'int':
-                o[rtype] = 'int'
-            return 'int'
-
-        if operator in ['+.', '-.', '*.','/.']:
-            if ltype in ['bool', 'int'] or rtype in ['bool', 'int']:
-                raise TypeMismatchInExpression(ctx)
-            if ltype != 'float':
-                o[ltype] = 'float'
-            if rtype != 'float':
-                o[rtype] = 'float'
-            return 'float'
-
-        if operator in ['>', '=']:
-            if ltype in ['bool', 'float'] or rtype in ['bool', 'float']:
-                raise TypeMismatchInExpression(ctx)
-            if ltype != 'int':
-                o[ltype] = 'int'
-            if rtype != 'int':
-                o[rtype] = 'int'
-            return 'bool'
-
-        if operator in ['>.', '=.']:
-            if ltype in ['bool', 'int'] or rtype in ['bool', 'int']:
-                raise TypeMismatchInExpression(ctx)
-            if ltype != 'float':
-                o[ltype] = 'float'
-            if rtype != 'float':
-                o[rtype] = 'float'
-            return 'bool'
-
-        if operator in ['&&', '||', '>b', '=b']:
-            if ltype in ['int', 'float'] or rtype in ['int', 'float']:
-                raise TypeMismatchInExpression(ctx)
-            if ltype != 'bool':
-                o[ltype] = 'bool'
-            if rtype != 'bool':
-                o[rtype] = 'bool'
-            return 'bool'
-            
-    def visitUnOp(self,ctx:UnOp,o):
-        operator = ctx.op
-        exp = self.visit(ctx.e,o)
-        if operator == '!':
-            if exp in ['float', 'int']:
-                raise TypeMismatchInExpression(ctx)
-            if exp != 'bool':
-                o[exp] = 'bool'
-            return 'bool'
-
-        if operator == '-.':
-            if exp in ['int', 'bool']:
-                raise TypeMismatchInExpression(ctx)
-            if exp != 'float':
-                o[exp] = 'float'
-            return 'float'
-
-        if operator == '-':
-            if exp in ['float', 'bool']:
-                raise TypeMismatchInExpression(ctx)
-            if exp != 'int':
-                o[exp] = 'int'
-            return 'int'
-
-        if operator == 'i2f':
-            if exp in ['float', 'bool']:
-                raise TypeMismatchInExpression(ctx)
-            if exp != 'int':
-                o[exp] = 'int'
-            return 'float'
-
-        if operator == 'floor':
-            if exp in ['int', 'bool']:
-                raise TypeMismatchInExpression(ctx)
-            if exp != 'float':
-                o[exp] = 'float'
-            return 'int'
-
-    def visitIntLit(self,ctx:IntLit,o):
-        return 'int'
-        
-    def visitFloatLit(self,ctx,o):
-        return 'float'
-        
-    def visitBoolLit(self,ctx,o):
-        return 'bool'
-        
-    def visitId(self,ctx,o):
-        if ctx.name in o:
-            return o[ctx.name]
-        raise UndeclaredIdentifier(ctx.name)
-
-#cau 2
-class StaticCheck(Visitor):
-    def visitProgram(self,ctx:Program,o):
-        o = {}
-        vardecl = [self.visit(x,o) for x in ctx.decl]
-        stmt = [self.visit(x,o) for x in ctx.stmts]
-        
-    def visitVarDecl(self,ctx:VarDecl,o):
-        if ctx.name in o:
+        if ctx.name in o[0]:
             raise Redeclared(ctx)
-        o[ctx.name] = ctx.name
+        o[0][ctx.name] = ctx.name
+        if ctx.name not in o[1]:
+            o[1][ctx.name] = ctx.name
+
+    def visitFuncDecl(self,ctx:FuncDecl,o):
+        if ctx.name in o[0]:
+            raise Redeclared(ctx)
+        o1 = [{},{}]
+        env = [self.visit(x,o1) for x in ctx.param]
+        dict_param = {}
+        for i in range(len(o1[0])):
+            dict_param[i] = o1[0][i]
+        o[0][ctx.name] = dict_param
+        var_inner = [x for x in ctx.local if type(x) == VarDecl]
+        func_inner = [x for x in ctx.local if type(x) == FuncDecl]
+        env1 = [self.visit(x,o1) for x in var_inner]
         
-    def visitBlock(self,ctx:Block,o):
-        o1 = {}
-        vardecl = [self.visit(x,o1) for x in ctx.decl]
+        o2 = o1.copy()
+        
         for keys, values in o.items():
-            if keys not in o1:
-                o1[keys] = values
-        stmt = [self.visit(x,o1) for x in ctx.stmts]
-        for keys, values in o1.items():
+            if keys not in o2:
+                o2[keys] = values
+                
+        o3 = [o1, o2]
+        
+        env2 = [self.visit(x,o3) for x in func_inner]
+        stmt = [self.visit(x,o2) for x in ctx.stmts]
+        for keys, values in o3.items():
             if keys in o and o[keys] == keys:
                 o[keys] = values
 
-    def visitAssign(self,ctx:Assign,o):
-        rtype = self.visit(ctx.rhs,o)
-        ltype = self.visit(ctx.lhs,o)
-        if ltype not in ['int', 'float', 'bool']:
-            if rtype not in ['int', 'float', 'bool']:
-                raise TypeCannotBeInferred(ctx)
-            o[ltype] = rtype
-            ltype = rtype
-        if rtype not in ['int', 'float', 'bool']:
-            o[rtype] = ltype
-            rtype = ltype
-        if ltype != rtype:
-            raise TypeMismatchInStatement(ctx)
-
-
-    def visitBinOp(self,ctx:BinOp,o):
-        operator = ctx.op
-        ltype = self.visit(ctx.e1,o)
-        rtype = self.visit(ctx.e2,o)
-        if operator in ['+', '-', '*','/']:
-            if ltype in ['bool', 'float'] or rtype in ['bool', 'float']:
-                raise TypeMismatchInExpression(ctx)
-            if ltype != 'int':
-                o[ltype] = 'int'
-            if rtype != 'int':
-                o[rtype] = 'int'
-            return 'int'
-
-        if operator in ['+.', '-.', '*.','/.']:
-            if ltype in ['bool', 'int'] or rtype in ['bool', 'int']:
-                raise TypeMismatchInExpression(ctx)
-            if ltype != 'float':
-                o[ltype] = 'float'
-            if rtype != 'float':
-                o[rtype] = 'float'
-            return 'float'
-
-        if operator in ['>', '=']:
-            if ltype in ['bool', 'float'] or rtype in ['bool', 'float']:
-                raise TypeMismatchInExpression(ctx)
-            if ltype != 'int':
-                o[ltype] = 'int'
-            if rtype != 'int':
-                o[rtype] = 'int'
-            return 'bool'
-
-        if operator in ['>.', '=.']:
-            if ltype in ['bool', 'int'] or rtype in ['bool', 'int']:
-                raise TypeMismatchInExpression(ctx)
-            if ltype != 'float':
-                o[ltype] = 'float'
-            if rtype != 'float':
-                o[rtype] = 'float'
-            return 'bool'
-
-        if operator in ['&&', '||', '>b', '=b']:
-            if ltype in ['int', 'float'] or rtype in ['int', 'float']:
-                raise TypeMismatchInExpression(ctx)
-            if ltype != 'bool':
-                o[ltype] = 'bool'
-            if rtype != 'bool':
-                o[rtype] = 'bool'
-            return 'bool'
-            
-    def visitUnOp(self,ctx:UnOp,o):
-        operator = ctx.op
-        exp = self.visit(ctx.e,o)
-        if operator == '!':
-            if exp in ['float', 'int']:
-                raise TypeMismatchInExpression(ctx)
-            if exp != 'bool':
-                o[exp] = 'bool'
-            return 'bool'
-
-        if operator == '-.':
-            if exp in ['int', 'bool']:
-                raise TypeMismatchInExpression(ctx)
-            if exp != 'float':
-                o[exp] = 'float'
-            return 'float'
-
-        if operator == '-':
-            if exp in ['float', 'bool']:
-                raise TypeMismatchInExpression(ctx)
-            if exp != 'int':
-                o[exp] = 'int'
-            return 'int'
-
-        if operator == 'i2f':
-            if exp in ['float', 'bool']:
-                raise TypeMismatchInExpression(ctx)
-            if exp != 'int':
-                o[exp] = 'int'
-            return 'float'
-
-        if operator == 'floor':
-            if exp in ['int', 'bool']:
-                raise TypeMismatchInExpression(ctx)
-            if exp != 'float':
-                o[exp] = 'float'
-            return 'int'
-
-    def visitIntLit(self,ctx:IntLit,o):
-        return 'int'
-        
-    def visitFloatLit(self,ctx,o):
-        return 'float'
-        
-    def visitBoolLit(self,ctx,o):
-        return 'bool'
-        
-    def visitId(self,ctx,o):
-        if ctx.name in o:
-            return o[ctx.name]
-        raise UndeclaredIdentifier(ctx.name)
-
-#cau 3
-class StaticCheck(Visitor):
-    def visitProgram(self,ctx:Program,o):
-        o = {}
-        vardecl = [self.visit(x,o) for x in ctx.decl]
-        stmt = [self.visit(x,o) for x in ctx.stmts]
-        
-    def visitVarDecl(self,ctx:VarDecl,o):
-        if ctx.name in o:
-            raise Redeclared(ctx)
-        o[ctx.name] = ctx.name
-
-    def visitFuncDecl(self,ctx:FuncDecl,o):
-    	if ctx.name in o:
-        	raise Redeclared(ctx)
-        o[ctx.name] = ctx.name
-        o1 = {}
-        env = [self.visit(x,o1) for x in ctx.param]
-        env1 = [self.visit(x,o1) for x in ctx.local]
-        env2 = [self.visit(x,o1) for x in ctx.stmts]
-
     def visitCallStmt(self,ctx:CallStmt,o):
-    	if ctx.name not in o:
+        if ctx.name not in o[0]:
             raise UndeclaredIdentifier(ctx.name)
-    	env = [self.visit(x,o) for x in ctx.args]
+        elif len(ctx.args) != len(o[0][ctx.name]):
+            raise TypeMismatchInStatement(ctx)
+
+        for i in range(len(ctx.args)):
+            if o[0][ctx.name][i] not in ['int', 'float', 'bool']:
+                o[0][ctx.name][i] = self.visit(ctx.args[i],o[0])
+            elif o[0][ctx.name][i] != self.visit(ctx.args[i],o[0]):
+                raise TypeMismatchInStatement(ctx)
 
     def visitAssign(self,ctx:Assign,o):
         rtype = self.visit(ctx.rhs,o)
@@ -316,11 +77,3 @@ class StaticCheck(Visitor):
         if ctx.name in o:
             return o[ctx.name]
         raise UndeclaredIdentifier(ctx.name)
-
-# Program([VarDecl("x"),
-# 	FuncDecl("foo",       [VarDecl("x")].       ,    []    ,         [Assign(Id("x"),FloatLit(2))].    )],
-# 	[Assign(Id("x"),       IntLit(3)),                 CallStmt("foo",[Id("x")])])
-
-
-
-
